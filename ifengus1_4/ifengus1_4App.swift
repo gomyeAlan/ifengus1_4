037f8f3055b0c9cacff13724b6c15a458c9d6ee5
Alan <gomye@qq.com>
2021-02-16 09:24:21 -0800

    Initial Commit

---
+++ ifengus1_4/ifengus1_4App.swift
@@ -0,0 +1,20 @@
+//
+//  ifengus1_4App.swift
+//  ifengus1_4
+//
+//  Created by Alan Yang on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ifengus1_4App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
