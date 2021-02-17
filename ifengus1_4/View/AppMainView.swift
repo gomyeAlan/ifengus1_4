eab6e43bb7cba57ad0cad606719bb7ad6946daec
wwwendydev <us68design@gmail.com>
2021-02-16 16:47:28 -0800

    调整大图数据--1

--- ifengus1_4/View/AppMainView.swift
+++ ifengus1_4/View/AppMainView.swift
@@ -8,36 +8,30 @@
 import SwiftUI
 
 struct AppMainView: View {
-
-        @State var isAccountViewPresented = false
-        @State var text: String = ""
-        
-      var body: some View {
+    @State var isAccountViewPresented = false
+    @State var text: String = ""
+    var body: some View {
         VStack {
-            
-
-        NavigationView{
-            VStack {
-            PublicSearchBar(text: $text)
-            TabView {
-                TodayView()
-                    .tabItem {
-                        Image(systemName: "note")
-                        Text("New")
+            NavigationView{
+                VStack {
+                    PublicSearchBar(text: $text)
+                    TabView {
+                        TodayView()
+                            .tabItem {
+                                Image(systemName: "note")
+                                Text("New")
+                            }
+                        MenuView()
+                            .tabItem {
+                                Image(systemName: "magnifyingglass")
+                                Text("Search")
+                            }
                     }
-                MenuView()
-                    .tabItem {
-                        Image(systemName: "magnifyingglass")
-                        Text("Search")
-                    }
-
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            .navigationBarItems(trailing: PublicAccountButton(isAccountViewPresented: $isAccountViewPresented))
+                    .navigationViewStyle(StackNavigationViewStyle())
+                    .navigationBarItems(trailing: PublicAccountButton(isAccountViewPresented: $isAccountViewPresented))
+                }
             }
         }
-        }
-      
       }
     }
 
